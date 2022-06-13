9798378834a6a088df5b8401f3ebb37f509142a7
Rajesh Y <Rajesh Y@DESKTOP-0IVRHT2>
2022-06-13 17:05:41 +0530

    first commit

---
+++ Day_7/src/com/tns/mypack/B.java
@@ -0,0 +1,13 @@
+package com.tns.mypack;
+import com.tns.pack.*;
+
+public class B {
+
+	public static void main(String[] args) {
+		A obj = new A();
+		c obj1 = new c();
+		obj.msg();
+		obj1.msg();
+	}
+
+}
---
+++ Day_7/src/com/tns/pack/c.java
@@ -0,0 +1,10 @@
+package com.tns.pack;
+
+public class c {
+
+	public void msg() {
+		System.out.println("hello from class C");
+
+	}
+
+}
