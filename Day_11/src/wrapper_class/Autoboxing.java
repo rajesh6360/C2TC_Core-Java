afd8c36d0e898fb1b33a81df80c3e23d6566e814
Rajesh Y <Rajesh Y@DESKTOP-0IVRHT2>
2022-06-18 20:22:42 +0530

    first commit

---
+++ Day_11/src/wrapper_class/Autoboxing.java
@@ -0,0 +1,13 @@
+package wrapper_class;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+		int a=20;
+		Integer i = Integer.valueOf(a);
+		Integer j = a;
+		System.out.println(a+" "+i+" "+j);
+
+	}
+
+}
---
+++ Day_11/src/wrapper_class/Wrapper_to_primitive.java
@@ -0,0 +1,15 @@
+package wrapper_class;
+
+public class Wrapper_to_primitive {
+
+	public static void main(String[] args) {
+		Integer a=new Integer(3);
+		int i=a.intValue();
+		int j=a;
+		System.out.println(a+" "+i+" "+j);
+		
+		
+
+	}
+
+}
