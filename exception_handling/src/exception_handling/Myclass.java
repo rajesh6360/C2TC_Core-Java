9fe338db493617b7baf2069fffd1afb5abcddc7a
Rajesh Y <Rajesh Y@DESKTOP-0IVRHT2>
2022-08-12 08:30:05 +0530

    first commit

---
+++ exception_handling/src/exception_handling/Myclass.java
@@ -0,0 +1,19 @@
+package exception_handling;
+
+import java.util.*;
+public class Myclass {
+	public static void main(String args[])
+
+	{
+	int x=10;
+
+	int y=20;
+
+	System.out.println(“welcome”);
+
+	int z=x1/y;
+
+	System.out.println(z);
+
+}
+}
