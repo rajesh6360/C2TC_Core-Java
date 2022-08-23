c696bdafadf2dfe6c637a60268d87b9c16e00a93
Rajesh Y <Rajesh Y@DESKTOP-0IVRHT2>
2022-08-23 18:36:28 +0530

    first commit

---
+++ JPAOneToOneBiDirectional/src/com/cg/jpastart/entities/Client.java
@@ -0,0 +1,42 @@
+package com.cg.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Client {
+
+	public static void main(String[] args) 
+	{
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		Student student = new Student();
+		student.setName("pavan");
+		
+		Address homeAddress = new Address();
+		homeAddress.setStreet("Tumkur road");
+		homeAddress.setCity("Bangalore");
+		homeAddress.setState("Karnataka");
+		homeAddress.setZipcode("560 057");
+		
+		//Injection of address to student
+		student.setAdress(homeAddress);
+		System.out.println("Student data added");
+		
+		//persist
+		em.persist(student);
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		factory.close();
+		
+		
+
+	}
+
+}
